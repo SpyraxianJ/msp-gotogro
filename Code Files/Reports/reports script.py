9f5bf34cd67e27968aa584dbe862b022c69ae882
Dylan Jarvis <jarvisdj@optusnet.com.au>
2022-10-18 22:27:45 +1100

    Sprint 2 - Day 7

--- Code Files/Reports/reports script.py
+++ Code Files/Reports/reports script.py
@@ -1,17 +1,33 @@
 #Generate PDF
-reportName = system.tag.read('[default]GotoGro/reportName').value + ".pdf"
-# Executes and distributes the report to save a PDF
-settings = {"path":"C:\\Users\\gotogro\\Desktop\\reports\\pdf", "fileName":reportName, "format":"pdf"}
-system.report.executeAndDistribute(path="Sales Report", project="GotoGro", action="save", actionSettings=settings)
-report.executeAndDistribute(path="Sales Report", project="GotoGro", action="save", actionSettings=settings)
+if event.source.parent.getComponent('report type').selected:
+	reportName = system.tag.read('[default]GotoGro/reportName').value + ".pdf"
+	# Executes and distributes the report to save a PDF
+	settings = {"path":"C:\\Users\\gotogro\\Desktop\\reports\\Member Sales Summary Report\\pdf", "fileName":reportName, "format":"pdf"}
+	system.report.executeAndDistribute(path="Member Sales Summary Report", project="GotoGro", action="save", actionSettings=settings)
+else:
+	reportName = system.tag.read('[default]GotoGro/reportName 2').value + ".pdf"
+	# Executes and distributes the report to save a PDF
+	settings = {"path":"C:\\Users\\gotogro\\Desktop\\reports\\Weekly Sales Report", "fileName":reportName, "format":"pdf"}
+	system.report.executeAndDistribute(path="Weekly Sales Report", project="GotoGro", action="save", actionSettings=settings)
+	
 
 #Generate CSV
-reportName = system.tag.read('[default]GotoGro/reportName').value + ".csv"
-# Executes and distributes the report to save a PDF
-settings = {"path":"C:\\Users\\gotogro\\Desktop\\reports\\csv", "fileName":reportName, "format":"csv"}
-system.report.executeAndDistribute(path="Sales Report", project="GotoGro", action="save", actionSettings=settings)
+if event.source.parent.getComponent('report type').selected:
+	reportName = system.tag.read('[default]GotoGro/reportName').value + ".csv"
+	# Executes and distributes the report to save a PDF
+	settings = {"path":"C:\\Users\\gotogro\\Desktop\\reports\\Member Sales Summary Report\\csv", "fileName":reportName, "format":"csv"}
+	system.report.executeAndDistribute(path="Member Sales Summary Report", project="GotoGro", action="save", actionSettings=settings)
+else:
+	reportName = system.tag.read('[default]GotoGro/reportName 2').value + ".csv"
+	# Executes and distributes the report to save a PDF
+	settings = {"path":"C:\\Users\\gotogro\\Desktop\\reports\\Weekly Sales Report", "fileName":reportName, "format":"csv"}
+	system.report.executeAndDistribute(path="Weekly Sales Report", project="GotoGro", action="save", actionSettings=settings)
+
+
 #View Reports
 system.net.openURL("https://drive.google.com/drive/folders/1-8J87bGnvLSu8psG_dR9KrML1PpTHN7t?usp=sharing")
 
 #filename logic
 reportName= "Member_" + {[.]selectedMemberID.value} + "_Sales_Report_" + dateFormat(now(1000), "yyyy-mm-dd hhmmss")
+
+reportname 2 = "Weekly_Sales_Report_" + dateFormat(now(1000), "yyyy-mm-dd hhmmss")
